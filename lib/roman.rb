db734f8032141f2aea5309493f40e7a562ccf81a
Pool Berrospi <poolb911@gmail.com>
2012-09-09 23:38:41 -0500

    commit 1

---
+++ lib/roman.rb
@@ -0,0 +1,9 @@
+class Roman
+  def convert veces
+			if veces == 1
+				retorno = "I"
+		end
+	#	retorno
+		
+	end
+end
---
+++ spec/roman_spec.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative '../lib/roman'
+
+describe Roman do
+	before do
+		@roman=Roman.new
+	end
+  it 'should convert 1 to I' do
+     @roman.convert(1).must_equal "I"
+  end
+
+end
